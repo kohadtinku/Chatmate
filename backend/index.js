b0034b5c35c6a367d77df27b25493c7be84df618
tinku kohad <tinkukohad05@gmail.com>
2025-11-14 11:10:39 +0530

    index fix

--- backend/index.js
+++ backend/index.js
@@ -31,12 +31,11 @@ app.use('/api/messages', messageRoute);
 connectDB();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-  app.get('/(.*)/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  }
-)
-;}
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('/*(.*)/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+  });
+}
 // ✅ Start server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
